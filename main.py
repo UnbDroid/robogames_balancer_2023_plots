8cd61da735cbc87153da729341a0ee109ea9ba59
Lucas <13675790+lschiavini@users.noreply.github.com>
2023-04-02 19:00:10 -0300

    separacao do codigo em funcoes

--- main.py
+++ main.py
@@ -22,33 +22,62 @@ column_names = [
  ]
 data.columns = column_names
 
-# Read the first row of the DataFrame
-first_row = data.iloc[0]
+def get_controller_gains(data):
+    # Read the first row of the DataFrame
+    first_row = data.iloc[0]
 
-# Create a dictionary to store the first row values with the column names as keys
-K = {}
-for column_name in column_names[:5]:
-    K[column_name] = first_row[column_name]
-# Print all K from the controller
-print("Controller Gains are:", K)
-controller_gains_string = f"Controller Gains: {K}"
+    # Create a dictionary to store the first row values with the column names as keys
+    K = {}
+    for column_name in column_names[:5]:
+        K[column_name] = first_row[column_name]
+    # Print all K from the controller
+    print("Controller Gains are:", K)
+    controller_gains_string = f"Controller Gains: {K}"
 
-# Drop the first row from the DataFrame
-data = data.drop(0)
+    # Drop the first row from the DataFrame
+    data = data.drop(0)
+
+    # Reset the index after dropping the first row
+    data.reset_index(drop=True, inplace=True)
+    return (controller_gains_string, data)
+def print_control_signals(columns_to_print, controller_gains_string, data):
+    fig, axes = plt.subplots(nrows=len(columns_to_print), ncols=1, figsize=(10, 24))
+
+    fig.suptitle(controller_gains_string, fontsize=16)
+
+    # Iterate through each column and create a line plot
+    for index, column_name in enumerate(printable_columns):
+        if column_name not in [
+            "Theta",
+            "Omega",
+            "Acceleration",
+            "RefPosition", 
+            "Velocity", 
+            "RefVelocity"
+        ]:
+            axes[index].plot(data[column_name], label=column_name)
+            axes[index].set_ylabel(column_name)
+            axes[index].legend()
+            axes[index].grid(True)
+    # Add some space between the subplots
+    fig.tight_layout(pad=3.0)
+def print_separate_graph_with_ref(column_name, ref_column_name):
+    fig2, ax2 = plt.subplots(figsize=(10, 6))
+    fig2.suptitle(controller_gains_string, fontsize=16)
+
+    ax2.plot(data[column_name], label=column_name)
+    ax2.plot(data[ref_column_name], label=ref_column_name)
+    ax2.set_ylabel(f'{column_name} / {ref_column_name}')
+    ax2.legend()
+    ax2.grid(True)
+
+controller_gains_string, data = get_controller_gains(data)
 
 # data = data.iloc[60000:100000]
-
-# Reset the index after dropping the first row
-data.reset_index(drop=True, inplace=True)
 
 data['AllSignals'] = data['uTheta'] + data['uOmega'] + data['uPosition'] + data['uVelocity'] + data['uAcceleration']
 
 new_colums = ["AllSignals"] + column_names
-
-# Create a figure with 7 subplots
-fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(10, 24))
-
-fig.suptitle(controller_gains_string, fontsize=16)
 
 printable_columns = [
     "uTheta", 
@@ -59,37 +88,9 @@ printable_columns = [
     "uAcceleration"
 ]
 
-# Iterate through each column and create a line plot
-for index, column_name in enumerate(printable_columns):
-    if column_name not in [
-        "Theta",
-        "Omega",
-        "Acceleration",
-        "RefPosition", 
-        "Velocity", 
-        "RefVelocity"
-    ]:
-        axes[index].plot(data[column_name], label=column_name)
-        axes[index].set_ylabel(column_name)
-        axes[index].legend()
-        axes[index].grid(True)
-
-def print_separate_graph_with_ref(column_name, ref_column_name):
-    fig2, ax2 = plt.subplots(figsize=(10, 6))
-    fig2.suptitle(controller_gains_string, fontsize=16)
-
-    ax2.plot(data[column_name], label=column_name)
-    ax2.plot(data[ref_column_name], label=ref_column_name)
-    ax2.set_ylabel(f'{column_name} / {ref_column_name}')
-    ax2.legend()
-    ax2.grid(True)
-   
-
+print_control_signals(printable_columns, controller_gains_string, data)
 print_separate_graph_with_ref('Position', 'RefPosition') 
 print_separate_graph_with_ref('Velocity', 'RefVelocity') 
-
-# Add some space between the subplots
-fig.tight_layout(pad=3.0)
 
 # Display the plot
 plt.show()
